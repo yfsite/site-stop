2f3f5f9851e93769113ce1d6eab5fc35a387169b
yfsite <63030086+yfsite@users.noreply.github.com>
2020-04-22 06:55:12 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="zh-tw" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>該網站暫停使用的公告</title>
+    <style media="screen">
+      #huanhang{
+        width:50%;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <center><h1>當你看到此頁，表示該網站暫停使用。</h1></center>
+    <br>
+    <center><h3>網站可能在維護，請稍後打開！</h3></center>
+    <br>
+    <center><h3>感謝您對EIVM的支持，我們會做的更好！</h3></center>
+    <br>
+    <center><div id=huanhang width=50%>&nbsp;<hr></div></center>
+    <center><p>Powered by eivm</p></center>
+    <center><p>YF blog : <a href=https://www.yfblog.ml>yfblog.ml</a>
+  </body>
+</html>
